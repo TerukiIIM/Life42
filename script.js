d45287e5d4fae8752d3c68a4724e0ecbd175a7b6
TerukiIIM <147390861+TerukiIIM@users.noreply.github.com>
2023-10-24 16:11:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+class player {
+    constructor(){
+        this.hand = []
+    }
+
+    discard() {
+        // ...
+    }
+    
+    useCard() {
+        // ...
+    }
+}
+
+class game {
+    constructor () {
+        this.deck = []
+        this.players = []
+    }
+
+    draw() {
+        // ...
+    }
+    
+    shovel() {
+        // ...
+    }
+}
+
+
+let player = new player()
+
+// Smile life => 200 cartes
+// Extention trash => 55 cartes
+// Extention girl power => 60 cartes
+// Extention apocalypse => 60 cartes
+// Extention fantastique => 60 cartes
+
+// Nombre de carte => 425 cartes
