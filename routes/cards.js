35265bfedadb90077bf0ca71a58a2563a7baa9ef
Jazzyunity (Edenskull) <113677546+Jazzyunity@users.noreply.github.com>
2023-10-26 13:02:37 +0200

    add reading json of cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const fs = require('fs');
+
 // middleware that is specific to this router
 router.use((req, res, next) => {
     console.log('Time: ', Date.now())
@@ -10,6 +12,12 @@ router.use((req, res, next) => {
 // define the home page route
 router.get('/', (req, res) => {
     res.send('Card page')
+})
+
+router.get('/getAllCards', (req, res) => {
+    fs.readFile('./config/cards.json', 'utf8', (err, jsonString) => {
+        res.json(JSON.parse(jsonString));
+    })
 })
 
 
